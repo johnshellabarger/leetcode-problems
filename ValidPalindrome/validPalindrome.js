09e90799450e5d0ed3815f732f152697c83c2f39
John Shellabarger <johnwilliamshellabarger@gmail.com>
2021-12-01 12:59:09 -0700

    palindrome fished! learning more regex

---
+++ ValidPalindrome/validPalindrome.js
@@ -0,0 +1,15 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+// Example 1:
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+var isPalindrome = function(s) {
+  let newString = s.replace(/[^A-Za-z0-9]/ig, '').toLowerCase()
+  let reversed = newString.split('').reverse().join('')  
+  return (newString === reversed)
+
+};
